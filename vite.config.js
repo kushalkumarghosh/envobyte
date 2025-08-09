5f0f1d6d862724a80a4487b87051aec8f668f15d
kushalkumarghosh <ghoshkushalcse@gmail.com>
2025-08-09 22:54:08 +0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VTTE_BASE_PATH || "/envobyte",
+  base: process.env.VTTE_BASE_PATH || "/",
 });
